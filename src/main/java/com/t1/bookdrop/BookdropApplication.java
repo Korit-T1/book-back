0619ca6d6fa34eb225b1eea99a5e386bf5d3ca68
rnurphy <ajvlzla0207@gmail.com>
2024-04-02 11:12:31 +0900

    first commit

---
+++ src/main/java/com/t1/bookdrop/BookdropApplication.java
@@ -0,0 +1,13 @@
+package com.t1.bookdrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookdropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookdropApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/t1/bookdrop/BookdropApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t1.bookdrop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookdropApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
