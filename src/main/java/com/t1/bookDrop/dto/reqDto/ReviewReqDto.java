f71be08c9cbebfb5d3cb8825a2396005a09f3c90
seowoo <dkr0210@naver.com>
2024-04-23 17:41:02 +0900

    feat:리뷰작성및호출

---
+++ src/main/java/com/t1/bookDrop/dto/reqDto/ReviewReqDto.java
@@ -0,0 +1,29 @@
+package com.t1.bookDrop.dto.reqDto;
+
+import com.t1.bookDrop.entity.Review;
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Data
+@Component
+public class ReviewReqDto {
+    private int reviewId;
+    private int bookId;
+    private int userId;
+    private String content;
+    private LocalDateTime createDate;
+    private LocalDateTime updateDate;
+
+    public Review toEntity() {
+        return Review.builder()
+                .reviewId(reviewId)
+                .bookId(bookId)
+                .userId(userId)
+                .content(content)
+                .createDate(createDate)
+                .updateDate(updateDate)
+                .build();
+    }
+}
---
+++ src/main/java/com/t1/bookDrop/service/ReviewService.java
@@ -0,0 +1,28 @@
+package com.t1.bookDrop.service;
+
+import com.t1.bookDrop.dto.reqDto.ReviewReqDto;
+import com.t1.bookDrop.dto.respDto.ReviewRespDto;
+import com.t1.bookDrop.entity.Review;
+import com.t1.bookDrop.repository.ReviewMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ReviewService {
+
+    @Autowired
+    private ReviewMapper reviewMapper;
+
+    @Transactional(rollbackFor = Exception.class)
+    public void saveReview(ReviewReqDto reviewReqDto) {
+        reviewMapper.saveReview(reviewReqDto.toEntity());
+    }
+
+    public List<ReviewRespDto> getReview(int bookId){
+        return reviewMapper.findReviewsAllByBookId(bookId).stream().map(Review::toDto).collect(Collectors.toList());
+    }
+}
