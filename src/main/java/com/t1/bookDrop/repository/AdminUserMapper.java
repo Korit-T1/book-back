8e0be676475f225b325d43b9426d885714620f53
jaeyoung3806 <jaeyoung3806@naver.com>
2024-05-12 00:06:09 +0900

    feat

--- src/main/java/com/t1/bookDrop/repository/AdminUserMapper.java
+++ src/main/java/com/t1/bookDrop/repository/AdminUserMapper.java
@@ -1,8 +1,16 @@
 package com.t1.bookDrop.repository;
 
+import com.t1.bookDrop.dto.reqDto.AdminUserReqDto;
+import com.t1.bookDrop.entity.AdminUser;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface AdminUserMapper {
-    public int findUser();
+    public List<AdminUser> findUser(
+            @Param("username") String username,
+            @Param("name") String name
+    );
 }
